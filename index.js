d889cfb6ae70b78d744c7ccb99f28830dbb42e89
nodirkhankhojayev <nodirkhankhojayev@gmail.com>
2024-06-09 21:17:28 +0500

    added js file

---
+++ index.js
@@ -0,0 +1,61 @@
+"u strict"
+// //  1-task  a) homework
+// let number = 4;
+// function oddEven(number){
+//     if(number % 2 == 0){
+//         console.log("juft");
+//     } else console.log("toq");
+// }
+// oddEven(number);
+
+// // 1-task b) homework
+// let nom = "5";
+// function identifyString(nom){
+//   if(nom === "5"){
+//     console.log("string");
+//   } else console.log("this is not string");
+// }
+// identifyString(nom);
+
+
+// //  1-task c)  homework
+// let year = 3;
+  
+// numOfMonth = year * 12;
+// console.log(numOfMonth);
+
+
+// let operator = document.querySelectorAll(".operator");
+
+// operator.addEventListener("click",()=>{
+//           console.log(operator.textcontent);
+// });
+
+let display = document.querySelector(".display");
+let button = document.querySelectorAll("button");
+let operator = document.querySelectorAll(".operator");
+
+button.forEach(button => {
+  button.addEventListener("click", () => {
+    display.value = button.textContent;
+  });
+});
+
+operator.forEach(operator =>{
+  operator.addEventListener("click", () =>{
+      for(let i = 0; i < operator.length; i++){
+        display.value = operator[i].textContent;
+      }   
+  })
+});
+
+
+// if(button.textContent === "=") {
+//   try {
+//       dispaly.value = eval(display.value); 
+//   } catch(error) {
+//       display.value = "Error"; 
+//   }
+// } else {
+//   display.value += button.textContent; 
+// }
